4180edc9a42ed805bde145b68d6900f160fdeaa6
JoelWalice <joelwalice@gmail.com>
2023-06-01 22:44:44 +0530

    Deploy

---
+++ app/quiz/data.js
@@ -0,0 +1,36 @@
+export const quiz = {
+    totalQuestion : 5,
+    questions: [
+        {
+            id:1,
+            question : 'Which is the Coldest continent in the World ? ',
+            answers : [ 'Asia','America','Africa','Antartica'],
+            correctAnswer : 'Antartica',
+        },
+        {
+            id:2,
+            question : 'What does the mammals,Whales and other creatures eats ? ',
+            answers : [ 'Small Fish','Ecna','Antartican Krill','Albatros'],
+            correctAnswer : 'Antartican Krill',
+        },
+        {
+            id:3,
+            question : 'Which is the Coldest Part of Antartica ? ',
+            answers : [ 'North Part','South Pole','Eastern','Western'],
+            correctAnswer : 'South Pole',
+        },
+        {
+            id:4,
+            question : 'Which among the following are Antartican Animals ?',
+            answers : [ 'Penquins','Seals','All of the Above','None of the Above'],
+            correctAnswer : 'None of the Above',
+        },
+        {
+            id:5,
+            question : 'When does Animals comes to Antartican Peninsula for breeding and feeding ?',
+            answers : [ 'Summer','Spring','Winter','Autumn'],
+            correctAnswer : 'Summer',
+        },
+    ]
+    
+}
